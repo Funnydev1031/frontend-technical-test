44db0ff1370fedc73375fb3537c73c70236dfef2
Toby <toby.willian.allen@outlook.com>
2024-07-24 20:40:21 -0400

    implement vehicle card and list styles

--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -1,40 +1,25 @@
-import React from 'react';
-import useData from './useData';
-import './style.scss';
+import React, { useEffect } from "react";
+import useFetch from "../../hooks/usefetch";
+import { VehicleCard } from "../vehicleCard";
+import styles from "./style.module.scss";
+import useData from "./useData";
 
 export default function VehicleList() {
-  // eslint-disable-next-line no-unused-vars
-  const [loading, error, vehicles] = useData();
+    const [loading, error, vehicles] = useData();
 
-  if (loading) {
-    return <div data-testid="loading">Loading</div>;
-  }
+    if (loading) {
+        return <div data-testid="loading">Loading</div>;
+    }
 
-  if (error) {
-    return <div data-testid="error">{ error }</div>;
-  }
-
-  return (
-    <div data-testid="results">
-      <p>List of vehicles will be displayed here</p>
-      <p>
-        Visit
-        <a href="/api/vehicles.json" target="_blank"> /api/vehicles.json</a>
-        {' '}
-        (main endpoint)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_fpace.json" target="_blank">/api/vehicle_fpace.json</a>
-        {' '}
-        (detail endpoint - apiUrl)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_xf.json" target="_blank">/api/vehicle_xf.json</a>
-        {' '}
-        (vehicle without any price)
-      </p>
-    </div>
-  );
+    if (error) {
+        return <div data-testid="error">{error}</div>;
+    }
+    return (
+        <div className={styles.vehicleList} data-testid="results">
+            {vehicles &&
+                vehicles.map((vehicle) => {
+                    return <VehicleCard key={vehicle.id} vehicle={vehicle} />;
+                })}
+        </div>
+    );
 }
